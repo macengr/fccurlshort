3874022fef144a82bbf3e68a41c4dde721f6d4c3
Scott <scottymac64@hotmail.com>
2016-07-18 04:22:40 +0000

    Final commit

--- server.js
+++ server.js
@@ -3,71 +3,113 @@
 var express = require('express');
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://localhost:27017/shortened';
-
-
-
-
+var url = require('url');
+var mongoUrl = 'mongodb://localhost:27017/shortened';
 var isUri = require('isuri');
 var shortid = require('shortid');
-
-
 var app = express();
 var toBeShort;
 var port = process.env.PORT || 8080;
 
+app.get('/new/*', function(request, response, next) {
+    
 
-app.use(function(request, response) {
-    
-    toBeShort = request.url;
-    toBeShort = toBeShort.slice(1);
-
-    
-    console.log("In comes a request to: " + request.url);
-    console.log("URL = " + toBeShort);
-    
+    var toBeShort = request.params[0];
+    console.log(toBeShort);
     
     var dude = isUri.isValid(toBeShort);
     console.log("This URL is " + dude);
     
+    var newURL = shortid.generate();
     
-    // check to see if it's a request for the home page
-    
-    if(request.url === "/") {
-      response.sendFile(process.cwd() + '/index.html');   
-    } else if(dude === true) {  // if it is a valid URL
-        
-        var newURL = shortid.generate();
-        
-
-        MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
-});
-
-
-
-        
-        
-        
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify( { "original_url" : toBeShort, "short_url": newURL }));
-    } else {   // Not Valid so outut Error message
-         response.writeHead(200, { 'Content-Type': 'application/json' });
-         response.end("ERROR - Invalid URL; Try again");
-    }
+    MongoClient.connect(mongoUrl, function (err, db) {
     	
+    	if (err) {
+    		console.log('Unable to connect to the mongoDB server. Error:', err);
+    	
+     	} else if (dude === true) {
+
+    		console.log('Connection established to', mongoUrl);
+ 
+    		var newOne = {original_url: toBeShort, short_url: newURL};
+    		
+    		db.collection('shorturls').insert(newOne, function (err, result) {
+    		    
+    			if (err) {
+    				console.log(err);
+    			} else {
+    				console.log('Inserted 1 document');
+    			}
+    			
+    			});
+
+          response.end(JSON.stringify( { "original_url" : toBeShort, "short_url": newURL } ));
+
+     	//Close connection
+        db.close();
+
+    } else {   // Not Valid so outut Error message
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end("ERROR - Invalid URL; Try again");
+    } 
+
+    });
 
 });
+
+
+app.get('/', function(request, response, next) {
+		
+      response.sendFile(process.cwd() + '/index.html');   
+
+});
+
+
+app.get('/*', function(request, response, next) {
+		
+
+     
+    	var getURL = request.params[0];
+    	console.log("shortened = " + getURL);
+    	
+    	
+    	// check db to see if short url exists
+    	
+    	MongoClient.connect(mongoUrl, function (err, db) {
+    	if (err) {
+    		console.log('Unable to connect to the mongoDB server. Error:', err);
+    	} else {
+    		console.log('Connection established to', mongoUrl);
+    		
+    		var collection = db.collection('shorturls');
+    	
+    		collection.findOne({"short_url": getURL}, function (err, doc){
+    				
+    		if (err) {
+    			console.log('Unable to connect to the mongoDB server. Error:', err);
+    		} else if (doc) {
+    			// found an entry in the DB, redirect the user to their destination
+    			response.redirect(doc.original_url);
+    		} else {
+    			// nothing found, error message
+    			response.writeHead(200, { 'Content-Type': 'application/json' });
+    			response.end(JSON.stringify("Invalid Shortcut; Try again"));
+      		}
+      	});
+
+  	}
+  	db.close();
+  	
+  	
+});
+		
+
+
+});
+
+
+
+ 
 
 app.listen(port, function () {
     console.log('Listening on port 8080...');
